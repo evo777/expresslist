6ba771ffbabf1ecd56b93560762c178438397e16
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-03-25 22:28:10 -0700

    Updated port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 // Port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Init app
 const app = express();
